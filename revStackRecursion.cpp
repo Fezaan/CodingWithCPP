102f4b7ab9450cdf871b62c044c8a34ef86231a8
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-05-09 23:35:34 +0530

    Day 5

---
+++ revStackRecursion.cpp
@@ -0,0 +1,28 @@
+void sortedInsert(stack<int> &s, int element)
+{
+    if (s.empty() || s.top() < element)
+    {
+        s.push(element);
+        return;
+    }
+
+    int num = s.top();
+    s.pop();
+    sortedInsert(s, element);
+    s.push(num);
+}
+
+void SortedStack ::sort()
+{
+    // Your code here
+
+    if (s.empty())
+    {
+        return;
+    }
+
+    int num = s.top();
+    s.pop();
+    sort();
+    sortedInsert(s, num);
+}
