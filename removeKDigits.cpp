70ef4b3d01ba5368046689cb2a1dc19689e68d24
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-06-17 15:20:09 +0530

    Day 36

---
+++ removeKDigits.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string removeKdigits(string s, int k) {
+        string res="";
+        stack<char> st;
+        if(s.length()==k)   return "0";
+        st.push(s[0]);
+        for(int i=1;i<s.length();i++){
+            while(k && !st.empty() && st.top()>s[i]){
+                k--;
+                st.pop();
+            }
+            st.push(s[i]);
+            if(st.size()==1 && s[i]=='0')   st.pop();
+        }
+        while(k && !st.empty()){
+            k--;
+            st.pop();
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        if(res.length()==0) return "0";
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        int K;
+        cin >> K;
+        Solution obj;
+        cout << obj.removeKdigits(S, K) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
