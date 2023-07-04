2bbac413dde0c3e8cbce548a296131a2aee5c925
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-07-04 21:22:25 +0530

    Day 79

---
+++ shortestJobFirst.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int solve(vector<int>& bt) {
+        // code here
+        sort(bt.begin(),bt.end());
+        int wait=0,cnt=bt.size();
+        for(int i=0;i<cnt;i++){
+            wait+=(bt[i]*(cnt-i-1));
+        }
+        return wait/cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> bt(n), p(n);
+        for (int i = 0; i < n; i++) {
+            cin >> bt[i];
+        }
+        Solution obj;
+        int ans = obj.solve(bt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
